cca7df611e6a8579d77870ce96007fa6ad758f08
rafaelcbernardino <rafaelg.bernardino@hotmail.com>
2024-04-25 19:44:47 -0300

    adicionando aula40 - exercitando classes 2

---
+++ secao4_aula40_exercicio_classes_2/Funcionario.cs
@@ -0,0 +1,9 @@
+﻿
+namespace secao4_aula40_exercicio_classes_2
+{
+    internal class Funcionario
+    {
+        public string nome;
+        public double salario;
+    }
+}
---
+++ secao4_aula40_exercicio_classes_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using secao4_aula40_exercicio_classes_2;
+using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario1, funcionario2;
+
+            funcionario1 = new Funcionario();
+            funcionario2 = new Funcionario();
+
+            //digitando dados do primeiro funcionário
+            Console.WriteLine("Dados do primeiro funcionário:");
+            Console.Write("Nome: ");
+            funcionario1.nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            funcionario1.salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //digitando dados do segundo funcionário
+            Console.WriteLine("Dados do segundo funcionário:");
+            Console.Write("Nome: ");
+            funcionario2.nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            funcionario2.salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double media = (funcionario1.salario + funcionario2.salario) / 2.00;
+            Console.WriteLine("Salário médio = " + media.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
