4b9c1a161af269df53c02c1c04ba023e7bf3b099
rafaelcbernardino <rafaelg.bernardino@hotmail.com>
2024-07-31 21:31:45 -0300

    Exercicios de fixacao - retangulo

---
+++ secao4Aula45Ex1/Program.cs
@@ -0,0 +1,19 @@
+﻿using secao4Aula45Ex1;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+            Console.WriteLine("Entre com a largura e altura do retângulo: ");
+            retangulo.altura = double.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com a largura e altura do retângulo: ");
+            retangulo.largura = double.Parse(Console.ReadLine());
+
+            Console.Write(retangulo);
+        }
+    }
+}
---
+++ secao4Aula45Ex1/Retangulo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace secao4Aula45Ex1
+{
+    public class Retangulo
+    {
+        public double largura;
+        public double altura;
+
+        public double Area()
+        {
+            return largura * altura;
+        }
+
+        public double Perimetro()
+        {
+            return largura * altura;
+        }
+
+        public double Diagonal()
+        {
+            return largura * altura;
+        }
+
+        public override string ToString()
+        {
+            return "Area = "
+                + Area().ToString("F2",CultureInfo.InvariantCulture)
+                + "\nPerímetro = "
+                + Perimetro().ToString("F2",CultureInfo.InvariantCulture)
+                + "\nDiagonal = "
+                + Diagonal().ToString("F2",CultureInfo.InvariantCulture);
+        }
+    }
+}
